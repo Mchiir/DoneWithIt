0e50535145db7a3cd1645ff097c7362905993ac2
Mchrispin <mchrispin14@gmail.com>
2025-10-10 16:44:44 +0200

    hello world commit

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello react-native devs, I'm Mchiir!</Text>
       <StatusBar style="auto" />
     </View>
   );
