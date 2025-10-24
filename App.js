c224955c2d31d90c1b5394d794c8978e2555aaa5
Mchrispin <mchrispin14@gmail.com>
2025-10-24 16:24:48 +0200

    done with tab nav

--- App.js
+++ App.js
@@ -3,21 +3,34 @@ import { Text, View } from 'react-native'
 
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 
 import HomeScreen from './screens/HomeScreen'
 import DetailsScreen from './screens/DetailsScreen'
 import ProfileScreen from './screens/ProfileScreen'
 
 const Stack = createNativeStackNavigator()
+const Tab = createBottomTabNavigator()
+
+function HomeStack() {
+  return (
+    // <NavigationContainer>
+    <Stack.Navigator initialRouteName='Home'>
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Details" component={DetailsScreen} />
+      {/* <Stack.Screen name="Profile" component={ProfileScreen} /> */}
+    </Stack.Navigator>
+    // </NavigationContainer>
+  )
+}
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='Home'>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Details" component={DetailsScreen} />
-        <Stack.Screen name="Profile" component={ProfileScreen} />
-      </Stack.Navigator>
+      <Tab.Navigator>
+        <Tab.Screen name="HomeTab" component={HomeStack} options={{ title: "Home" }} />
+        <Tab.Screen name="Profile" component={ProfileScreen} />
+      </Tab.Navigator>
     </NavigationContainer>
   )
 }
